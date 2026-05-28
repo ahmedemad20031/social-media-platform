3440ce4a3ed7ac9be9cdf21da6d879f14d766ce5
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-05-28 15:10:47 +0200

    fix paths for railway

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const Connetion = async function () {
   try {
-    const Url = "mongodb://localhost:27017/Social";
+    const Url = process.env.MONGO_URI;
+
     await mongoose.connect(Url);
+
     console.log("database Connected");
   } catch (error) {
     console.log(error);
   }
 };
+
 module.exports = { Connetion };
