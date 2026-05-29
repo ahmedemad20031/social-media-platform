63b00629da183d2a083abb8b9f4842910a88f314
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-05-29 11:48:21 +0200

    add root route

--- app.js
+++ app.js
@@ -1,85 +1,53 @@
-// =====================
-// dotenv (لازم أول سطر)
-// =====================
-const dotenv = require("dotenv");
-dotenv.config();
-
-// =====================
-// req express
-// =====================
+//req express
 const express = require("express");
-const http = require("http");
-const path = require("path");
-const cors = require("cors");
 
-// =====================
-// socket
-// =====================
+const http = require("http");
+const { Server } = require("socket.io");
+
 const { initSocket } = require("./utils/socket");
 
-// =====================
-// app + server
-// =====================
 const app = express();
+
 const server = http.createServer(app);
 
-// =====================
-// DB connection
-// =====================
-const connectDB = require("./Config/dbConfig");
-
-// =====================
-// socket init
-// =====================
 initSocket(server);
 
-// =====================
-// TEST ENV (بعد dotenv)
-// =====================
-console.log("ENV TEST:", {
-  MONGO_URI: process.env.MONGO_URI ? "OK" : "MISSING",
-  PORT: process.env.PORT,
-});
+const path = require("path");
 
-// =====================
-// middlewares
-// =====================
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+//data base
+const { Connetion } = require("./Config/dbConfig");
+
+//dotenv
+const dotenv = require("dotenv").config();
+
+//port
+const port = process.env.PORT || 3000;
+
+//cors
+const cors = require("cors");
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// =====================
-// Routes
-// =====================
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.use("/api/v1/auth", require("./Routes/AuthRoutes"));
 app.use("/api/v1/post", require("./Routes/PostRoutes"));
+// app.use("/api/v1/user", require("./Routes/UserRoutes"));
 app.use("/api/v1/chat", require("./Routes/ChatRoutes"));
 app.use("/api/v1/message", require("./Routes/MessageRoutes"));
 app.use("/api/v1/follow", require("./Routes/FollowRoutes"));
 app.use("/api/v1/suggest", require("./Routes/SuggestRoutes"));
 app.use("/api/v1/notifications", require("./Routes/NotificatioRoutes"));
 
-// =====================
-// DB connect (بعد dotenv)
-// =====================
-connectDB();
-
-// =====================
-// PORT
-// =====================
-const port = process.env.PORT || 3000;
-
-// =====================
-// server start
-// =====================
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+Connetion();
