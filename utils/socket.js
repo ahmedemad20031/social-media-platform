7b448a0b36245a433576c1d683fdffaeed25bda4
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-05-31 01:09:46 +0200

    fix cors production

--- utils/socket.js
+++ utils/socket.js
@@ -4,7 +4,7 @@ let io;
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://social-media-platform-frontend-five.vercel.app",
       credentials: true,
     },
   });
