03e66755fb0d806d68506fa9db4bfdc16525397b
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-06-02 22:58:35 +0200

    feat: switch email relay to brevo to bypass railway outbound block

--- utils/MailService.js
+++ utils/MailService.js
@@ -1,12 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com", // سيرفر Brevo العالمي المفتوح على ريلواي
-  port: 587, // البورت المعتمد والشغال دايماً
-  secure: false, // false للـ TLS
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: "ahmed.emad.soliman.me@gmail.com", // سيب إيميلك هنا عادي
-    pass: process.env.BREVO_KEY, // ⚠️ المفتاح السحري اللي هنجيبه حالا
+    user: "ad5b9a001@smtp-brevo.com",
+    pass: process.env.BREVO_PASS,
   },
   tls: {
     rejectUnauthorized: false,
@@ -16,12 +16,12 @@ const transporter = nodemailer.createTransport({
 async function sendemail({ to, subject, text }) {
   try {
     const info = await transporter.sendMail({
-      from: '"Social Media Platform" <ahmed.emad.soliman.me@gmail.com>', // إيميلك الموثق
+      from: '"Social Media Platform" <ahmed.emad.soliman.me@gmail.com>',
       to: to,
       subject: subject,
       text: text,
     });
-    console.log("🔥 OTP Sent via Brevo Successfully!");
+    console.log("🔥 OTP Sent via Brevo successfully to:", to);
   } catch (err) {
     console.log("❌ Error to send email:", err.message);
   }
