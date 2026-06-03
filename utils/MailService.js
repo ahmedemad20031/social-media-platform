e777b9f99e1d1787df4a82927accaa5741848afc
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-06-03 19:00:33 +0200

    fix sendemail

--- utils/MailService.js
+++ utils/MailService.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: "ahmed.emad.soliman.me@gmail.com",
     pass: process.env.APP_PASS,
